fea477dcad6aab534f859911e069c6058bd46df0
Alejandro Gonzalez Velasquez <alejo.gv06@gmail.com>
2023-01-19 16:35:00 -0500

    Cambios para JS

--- JS/formulario.js
+++ JS/formulario.js
@@ -1,1 +1,26 @@
 
+
+function check(){
+
+client_name = document.getElementById("exampleInputName1").value;
+client_email = document.getElementById("exampleInputEmail1").value;
+client_phone = document.getElementById("exampletelefone1").value;
+client_country = document.getElementById("pais_select").value; 
+
+
+
+   if (client_name != "" && client_email != "" && client_phone != "" && client_country != "") {
+
+   
+
+   }else {
+
+           console.log("Registro completado, biendevenido "+client_name)
+           
+        }
+           
+
+ 
+}
+
+
