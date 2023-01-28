55683ae0dd5b8e96d64619b7ccb902930858b414
Alejandro Gonzalez Velasquez <alejo.gv06@gmail.com>
2023-01-28 10:25:31 -0500

    Cambios 1.2

--- JS/formulario.js
+++ JS/formulario.js
@@ -2,16 +2,14 @@
 
 
 
-
-function checkinfo(){
-
+ 
+ 
+function checkinfo(){    
 
 client_name = document.getElementById("exampleInputName1").value;
 client_email = document.getElementById("exampleInputEmail1").value;
 client_phone = document.getElementById("exampletelefone1").value;
-client_country = document.getElementById("pais_select").value; 
-
-
+client_country = document.getElementById("pais_select").value;
 
    if (client_name != "" && client_email != "" && client_phone != "" && client_country != "") {
 
@@ -24,6 +22,26 @@ client_country = document.getElementById("pais_select").value;
    }   
 
   
+}
+
+
+function suscribe() {
+
+     client_name = document.getElementById("exampleInputName1").value;
+     client_email = document.getElementById("exampleInputEmail1").value;
+     client_phone = document.getElementById("exampletelefone1").value;
+     client_country = document.getElementById("pais_select").value;
+
+     var Arrayalmacen = [client_name,client_email,client_phone,client_country];
+
+     for (i = 0; i > 4; i++){
+
+      
+
+
+     }
+
+
 }
 
 
